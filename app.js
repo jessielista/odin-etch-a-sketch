da556005b2e8d1efe5fe589c019f878d6235e7fa
jessielista <jmgonzalez8282@gmail.com>
2022-03-27 21:30:36 -0700

    Creates rows

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// Need to make DOM that replciates the columns and rows
+const container = document.querySelector('.container')
+
+function createDivs(columns, rows) {
+  for (let i = 0; i < columns * rows; i++) {
+    const div = document.createElement('div')
+    // div.style.border = '1px solid green'
+    // container.getElementsByClassName.gridTemplateColumns = `repeat(${columns}, ifr)`
+    // container.getElementsByClassName.gridTemplateRows = `repeat(${rows}, ifr)`
+    div.setAttribute('class', 'showBox')
+    container.appendChild(div)
+  }
+}
+
+createDivs(2, 2)
